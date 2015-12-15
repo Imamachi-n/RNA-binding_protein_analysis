665b920aa9d23bfba2f2205df477b34adc453211
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2015-12-15 18:43:11 +0900

    no message

--- PAR-CLIP_data_analysis/PARalyzer_data_analysis.sh
+++ PAR-CLIP_data_analysis/PARalyzer_data_analysis.sh
@@ -12,6 +12,10 @@
 #csv2bed6.py
 #bed6tobed12.py
 #A_define_Representative_isoform_from_RNA-seq.py
+#Define_motif_sites_on_transcriptome.py
+#Compare_annotation_infor_with_CLIP-seq_peaks.py
+
+#Bedtools(intersectBed)
 
 ##dataset##
 #SRR309285_4SU_DMEM_PAR-CLIP_HuR_4_result_ver3.bowtie(Default bowtie output)
@@ -31,7 +35,7 @@ ScriptDir='/home/akimitsu/software/PARalyzer_v1_1/scripts'
 ###PARalyzer Result Directory###
 bowtiefile=`basename ${BowtieOutput} .bowtie`
 savedir=PARalyzer_result_${bowtiefile}_${flg}
-#mkdir ${savedir} 
+mkdir ${savedir} 
 
 ###Estimate Conversion pattern###
 #python3 ${ScriptDir}/estimate_conversion_pattern.py ${bowtiefile}.bowtie ./${savedir}/${bowtiefile}_conversion.result
@@ -47,19 +51,18 @@ savedir=PARalyzer_result_${bowtiefile}_${flg}
 #python3 ${ScriptDir}/csv2bed6.py ./${savedir}/${bowtiefile}_clusters.csv ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters.fasta ./${savedir}/${bowtiefile}_clusters.result
 
 ###Convert Bed6 into Bed12 file###
-#python3 ${ScriptDir}/bed6tobed12.py ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters_B12.bed
+python3 ${ScriptDir}/bed6tobed12.py ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters_B12.bed
 
 ###Define representative isoform from RNA-seq data(Cuffdiff results)###
 CuffdiffGeneFilename=`basename ${CuffdiffGeneResultData} .diff`
-#python3 ${ScriptDir}/A_define_Representative_isoform_from_RNA-seq.py ${CuffdiffIsoformData} ${CuffdiffGeneResultData} ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt
+python3 ${ScriptDir}/A_define_Representative_isoform_from_RNA-seq.py ${CuffdiffIsoformData} ${CuffdiffGeneResultData} ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt
 
 ###Extract CLIP-seq peaks in 3'UTR region of RefSeq transcripts###
-#intersectBed -a ${Refseq3UTR} -b ./${savedir}/${bowtiefile}_clusters_B12.bed -wa -wb > ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed
-#python3 ${ScriptDir}/Define_motif_sites_on_transcriptome.py ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_trx_sites.txt
+intersectBed -a ${Refseq3UTR} -b ./${savedir}/${bowtiefile}_clusters_B12.bed -wa -wb > ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed
+python3 ${ScriptDir}/Define_motif_sites_on_transcriptome.py ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_trx_sites.txt
 python3 ${ScriptDir}/Compare_annotation_infor_with_CLIP-seq_peaks.py ./${savedir}/${bowtiefile}_trx_sites.txt \
 																	 ./${savedir}/${bowtiefile}_clusters.result \
 																	 ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt \
 																	 ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.txt \
 																	 ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.fasta \
 																	 ./${savedir}/${bowtiefile}_rep_trx_3UTR.bed
-#python3 ${ScriptDir}/Compare_annotation_infor_with_CLIP-seq_peaks_for_bed.py ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_rep_trx_3UTR.bed
