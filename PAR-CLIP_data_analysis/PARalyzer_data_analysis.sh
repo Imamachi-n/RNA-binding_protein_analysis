cf0c24011f5d165f7da12ee05d32d538f836b600
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2015-12-15 11:02:07 +0900

    no message

--- PAR-CLIP_data_analysis/PARalyzer_data_analysis.sh
+++ PAR-CLIP_data_analysis/PARalyzer_data_analysis.sh
@@ -37,23 +37,29 @@ savedir=PARalyzer_result_${bowtiefile}_${flg}
 #python3 ${ScriptDir}/estimate_conversion_pattern.py ${bowtiefile}.bowtie ./${savedir}/${bowtiefile}_conversion.result
 
 ###Make .ini file for PARalyzer###
-python3 ${ScriptDir}/make_ini_file_for_PARalyzer.py ${bowtiefile} ${Genome2bitFile} ${MinConversionLocations} 5 ./${savedir}/${bowtiefile}
+#python3 ${ScriptDir}/make_ini_file_for_PARalyzer.py ${bowtiefile} ${Genome2bitFile} ${MinConversionLocations} 5 ./${savedir}/${bowtiefile}
 
 ###Run PARalyzer###
 #echo ./${savedir}/${bowtiefile}.ini
 #PARalyzer ./${savedir}/${bowtiefile}.ini
 
 ###Convert CSV file into Bed6 file###
-python3 ${ScriptDir}/csv2bed6.py ./${savedir}/${bowtiefile}_clusters.csv ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters.fasta ./${savedir}/${bowtiefile}_clusters.result
+#python3 ${ScriptDir}/csv2bed6.py ./${savedir}/${bowtiefile}_clusters.csv ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters.fasta ./${savedir}/${bowtiefile}_clusters.result
 
 ###Convert Bed6 into Bed12 file###
-python3 ${ScriptDir}/bed6tobed12.py ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters_B12.bed
+#python3 ${ScriptDir}/bed6tobed12.py ./${savedir}/${bowtiefile}_clusters.bed ./${savedir}/${bowtiefile}_clusters_B12.bed
 
 ###Define representative isoform from RNA-seq data(Cuffdiff results)###
 CuffdiffGeneFilename=`basename ${CuffdiffGeneResultData} .diff`
-python3 ${ScriptDir}/A_define_Representative_isoform_from_RNA-seq.py ${CuffdiffIsoformData} ${CuffdiffGeneResultData} ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt
+#python3 ${ScriptDir}/A_define_Representative_isoform_from_RNA-seq.py ${CuffdiffIsoformData} ${CuffdiffGeneResultData} ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt
 
 ###Extract CLIP-seq peaks in 3'UTR region of RefSeq transcripts###
-intersectBed -a ${Refseq3UTR} -b ./${savedir}/${bowtiefile}_clusters_B12.bed -wa -wb > ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed
-python3 ${ScriptDir}/Define_motif_sites_on_transcriptome.py ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_trx_sites.txt
-python3 ${ScriptDir}/Compare_annotation_infor_with_CLIP-seq_peaks.py ./${savedir}/${bowtiefile}_trx_sites.txt ./${savedir}/${bowtiefile}_clusters.result ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.txt ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.fasta
+#intersectBed -a ${Refseq3UTR} -b ./${savedir}/${bowtiefile}_clusters_B12.bed -wa -wb > ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed
+#python3 ${ScriptDir}/Define_motif_sites_on_transcriptome.py ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_trx_sites.txt
+python3 ${ScriptDir}/Compare_annotation_infor_with_CLIP-seq_peaks.py ./${savedir}/${bowtiefile}_trx_sites.txt \
+																	 ./${savedir}/${bowtiefile}_clusters.result \
+																	 ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt \
+																	 ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.txt \
+																	 ./${savedir}/${bowtiefile}_trx_sites_for_NGS_dataset.fasta \
+																	 ./${savedir}/${bowtiefile}_rep_trx_3UTR.bed
+#python3 ${ScriptDir}/Compare_annotation_infor_with_CLIP-seq_peaks_for_bed.py ./${savedir}/${CuffdiffGeneFilename}_rep_isoform_list.txt ./${savedir}/${bowtiefile}_vs_RefSeq_3UTR.bed ./${savedir}/${bowtiefile}_rep_trx_3UTR.bed
--- PAR-CLIP_data_analysis/scripts/Compare_annotation_infor_with_CLIP-seq_peaks.py
+++ PAR-CLIP_data_analysis/scripts/Compare_annotation_infor_with_CLIP-seq_peaks.py
@@ -2,14 +2,16 @@
 
 import sys
 
-clip_seq_pos = open(sys.argv[1], 'r')
-cluster_result = open(sys.argv[2], 'r')
-rep_infor = open(sys.argv[3], 'r')
+clip_seq_pos = open(sys.argv[1], 'r') #${bowtiefile}_trx_sites.txt
+cluster_result = open(sys.argv[2], 'r') #${bowtiefile}_clusters.result
+rep_infor = open(sys.argv[3], 'r') #${CuffdiffGeneFilename}_rep_isoform_list.txt
 
-output_file = open(sys.argv[4], 'w')
-fasta_file = open(sys.argv[5], 'w')
+output_file = open(sys.argv[4], 'w') #${bowtiefile}_trx_sites_for_NGS_dataset.txt
+fasta_file = open(sys.argv[5], 'w') #${bowtiefile}_trx_sites_for_NGS_dataset.fasta
+bed_file = open(sys.argv[6], 'w') #./${savedir}/${bowtiefile}_rep_trx_3UTR.bed
 
 seq_dict = {}
+seq_bed_dict = {}
 
 for line in cluster_result:
     line = line.rstrip()
@@ -17,8 +19,10 @@ for line in cluster_result:
     seq = data[6]
     name = data[3]
     seq_dict[name] = seq
+    seq_bed_dict[name] = data[0:6]
 
 clip_dict = {}
+clip_bed_dict = {}
 
 for line in clip_seq_pos:
     line = line.rstrip()
@@ -29,11 +33,14 @@ for line in clip_seq_pos:
     if int(st) < 0:
         continue
     seq = seq_dict[name]
+    seq_bed_infor = seq_bed_dict[name]
     infor = st + '|' + seq
     if not refid in clip_dict:
         clip_dict[refid] = [infor]
+        clip_bed_dict[refid] = [seq_bed_infor]
     else:
         clip_dict[refid].append(infor)
+        clip_bed_dict[refid].append(seq_bed_infor)
 
 for line in rep_infor:
     line = line.rstrip()
@@ -51,12 +58,19 @@ for line in rep_infor:
         motif_num = len(infor)
         print(line,st_line,seq_line,motif_num, sep="\t", end="\n", file=output_file)
 
+        bed_infor = clip_bed_dict[refid]
+
         for index in range(len(st_infor)):
+            ###For fasta file###
             st_index = st_infor[index]
             name_index = data[1] + '|' + refid + '|' + str(st_index)
             seq_index = seq_infor[index]
             print('>',name_index, sep="",end="\n",file=fasta_file)
             print(seq_index, sep="\t",end="\n",file=fasta_file)
+
+            ###For bed file###
+            bed_index = bed_infor[index]
+            print("\t".join(bed_index), sep="\t",end="\n",file=bed_file)
         continue
     else:
         print(line,'NA','NA','NA', sep="\t",end="\n",file=output_file)
